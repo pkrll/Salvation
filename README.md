f38828fd0eaf4db1501c34f821f4ea489a2b79dc
Ardalan Samimi <ardalan.samimi@gmail.com>
2015-08-01 17:46:45 +0200

    Improved code, added max/min options for length
    Changes 0.3.0:
       * Added max/min options for data-length attribute.
       * Improved code, to iterate over the form elements as few times as
    possible while gathering the ones to validate.
       * Also, changed validation attribute to “data-validate”, while using
    data-format for the format options.
       * Callback function for invalid elements now checks if the field is
    valid according to the rules before removing red border.

--- README.md
+++ README.md
@@ -1,30 +1,76 @@
 ### Salvation
-Work in progress.
+A lightweight, easy to use form validation tool, written entirely in JavaScript and with no library dependencies. Also, a work in progress.
 
-#### Usage
-* Create the form and add the validation rules (a complete list of attributes will be added as project progresses.
+### Usage
+Using Salvation is easy and requires very little effort to take advantage of the built-in validation rules. Below follows a quick start guide:
+* Create the form and add the validation type (the attribute data-validate):
 ```html
 <form id="form">
-    <p><input type="text" required="required"></p>
-    <p><input type="password" required="required"></p>
-    <p><input type="text" data-format="numeric"></p>
-    <p><input type="text" data-format="alphanumeric" data-length="1,5"></p>
-    <p><input type="text" data-length="5"></p>
+    <p><input type="text" name="username" data-validate="required"></p>
+    <p><input type="password" name="password" required="required"></p>
+    <p><input type="text" name="income" data-validate="numeric" data-length="5" data-format="min"></p>
+    <p><input type="text" name="some-other-field" data-validate="alphanumeric" data-length="1,5"></p>
+    <p><input type="text" name="date" data-validate="date" data-format="YY/MM/DD"></p>
     <p><input type="submit" /></p>
 </form>
-
 ```
 * Add the following to the bottom of the page.
 ```js
 <script type="text/javascript">
-(function() {
-
      var salvation = new Salvation({
          element: document.getElementById("form")
      });
-
-})();
 </script>
+```
+#### Options
+##### Properties
+```js
+    new Salvation({
+        element         :   document.getElementById("element"),
+        dateFormat      :   "MM/DD/YYYY",
+        datePlaceholder :   true
+    });
+```
+* `element`: The form element to validate (**required**).
+* `dateFormat`: The default validation rule for the date fields. This setting will be used if the attribute *data-format* is not set. If the `data-format` is set on one element, the default format will be overridden (`default: mm/dd/yyyy`).
+* `datePlaceholder`: If set true, the date fields will display which date format they are set to (`default: true`).
+
+##### Attributes
+```html
+    <input type="text" data-validate="required"></p>
+    <input type="text"data-validate="numeric" data-length="5" data-format="min">
+    <input type="text" data-length="1,">
+    <input type="text" data-validate="date" data-format="DD/MM/YYYY">
+</form>
+```
+* `data-validate`: Sets the validation rule. The built-in rules are as follows:
+   * `required`: The field can not be empty (you can get the same effect by using the HTML5 ``required`` attribute, as the plugin also includes these, but please note that modern browsers already check for those).
+   * `numeric`: The field can only contain numbers.
+   * `alphanumeric`: The field can only contain letters, numbers and underscore.
+   * `date`: The field must be a valid date, formatted as specified by the data-format attribute (or the default formatting rule). Salvation will check upon validation if the inputed date actually exists.
+   * You can also create your own rules by setting a custom value to `data-validate` and passing a custom regular expression when initializing the plugin (see below for an example).
+* `data-length`: Specifies a minimum and/or maximum length, delimited by a comma (i.e. `data-length="5,10"`).
+   * `5`: A single digit will set the maximum length.
+   * `5,`: A single digit, followed by a comma, sets the minimum length.
+* `data-format`: Formatting option for the date fields (i.e. `data-format="YY/MM/DD"`). This will override the default format. Can also be used to set the value of the length attribute to be either a maximum or minimum limit (`data-format="min/max"`). The `data-length` attribute must in that case only contain digits.
+
+#### Customization
+Salvation is also easily extendable and can take custom validation rules (as regular expressions). Below follows an example where we want to create a password field that requires at least one digit and a special character:
+```html
+    <form id="form">
+        <input type="password" name="password" data-validate="custom-password">
+    </form>
+
+    <script type="text/javascript">
+        var salvation = new Salvation({
+            element: document.getElementById("form")
+        }, {
+            custom-password: /(?=.*[0-9])(?=.*[\W])/
+            // The custom rules must be a regular expression
+            // and have the same name as specified in the
+            // form element.
+        });
+    </script>
 ```
 #### Author
 Ardalan Samimi
--- src/salvation.js
+++ src/salvation.js
@@ -4,39 +4,71 @@
  *
  * Validates forms.
  *
- * @version 0.2.1
+ * @version 0.3.0
  * @author Ardalan Samimi
  */
 (function(window) {
     // The default settings
     var defaults = {
-        element:    false
+        element         : false,
+        dateFormat      : "MM/DD/YYYY",
+        datePlaceholder : true
     }
-
-    Salvation = function (options) {
-        this.settings = this.extend(defaults, options);
-
-        this.validationFailedStyle = "wrong";
-
-        this.element    = this.settings.element;
-        this.children   = this.getElementsByTagName(this.element, "input");
-        this.required   = this.getElementsByAttribute(this.children, "required");
-        this.specials   = {
-            date:         this.getElementsByAttribute(this.children, "data-date"),
-            length:         this.getElementsByAttribute(this.children, "data-length"),
-            numeric:        this.getElementsByAttribute(this.children, "data-format", "numeric"),
-            alphanumeric:   this.getElementsByAttribute(this.children, "data-format", "alphanumeric")
+    // The default patterns
+    var patterns = {
+        length          :'^(.{X,Y}$)',
+        numeric         :/^(\d)+$/,
+        alphanumeric    :/^([\d|\w])+$/
+    }
+    /**
+     * Initialize the Salvation plugin.
+     *
+     * @param       object  The settings
+     * @returns     object  An instance of Salvation
+     */
+    Salvation = function (options, userPatterns) {
+        // Auto-instantiates if the object was
+		// not called with the new-keyword.
+        if (this instanceof Salvation) {
+            var self = this;
+            // Extend the default settings, but
+            // exit if the element is not set.
+            self.patterns = self.extend(patterns, userPatterns);
+            self.settings = self.extend(defaults, options);
+            if (self.settings.element === false) return null;
+            self.element = self.settings.element;
+            // Gather all the validation elements and
+            // bind the submit event to validate.
+            self.gatherElements();
+            self.bindEvents();
+            // It's common courtesy for date fields to
+            // display which format they should be in,
+            // but only if the plugin user thinks so.
+            if (this.settings.datePlaceholder)
+                self.specials.date.map(function(element) {
+                    var format = (element.getAttribute("data-format") === null) ? self.settings.dateFormat : element.getAttribute("data-format");
+                    element.setAttribute("placeholder", format);
+                });
+            self.validationFailedStyle = "wrong";
+        } else {
+            return new Salvation(settings);
         }
-        this.patterns   = {
-            length:         '^(.{X,Y}$)',
-            numeric:        /^(\d)+$/,
-            alphanumeric:   /^([\d|\w])+$/
-        }
-
-        this.bindEvents();
     }
 
     Salvation.prototype = {
+        /**
+         * Gather all the children elements of the form,
+         * and sort them according to their types, i.e.
+         * all date elements inside this.specials.date.
+         *
+         * @returns     void
+         */
+        gatherElements: function() {
+            this.children = this.getElementsByTagName(this.element, "input");
+            this.required = this.getElementsByAttributes(this.children, "required,data-required");
+            this.specials = this.getElementsByAttributeAsObject(this.children, "data-validate");
+            this.specials.length    = this.getElementsByAttribute(this.children, "data-length");
+        },
         /**
          * Extend the default options with
          * user inputed ones.
@@ -85,9 +117,11 @@
             // this way to keep the references. Removes both the
             // invalid class and event listener after it's called.
             var callback = function() {
-                if (this.classList.contains(self.validationFailedStyle))
-                    this.classList.remove(self.validationFailedStyle);
-                this.removeEventListener("keyup", arguments.callee, false);
+                if (self.checkElementByPattern(this)) {
+                    if (this.classList.contains(self.validationFailedStyle))
+                        this.classList.remove(self.validationFailedStyle);
+                    this.removeEventListener("keyup", arguments.callee, false);
+                }
             }
             // The validation process
             this.element.addEventListener("submit", function (event) {
@@ -126,13 +160,14 @@
          * to also go by the attribute value.
          *
          * @param       elements    Array of elements to search
-         * @param       string      Attribute to find
          * @param       string      Value to find
          * @returns     array       List of elements found
          */
         getElementsByAttribute: function (elements, attribute, value) {
             var foundElements = [], value = value || null;
             for (i = 0; i < elements.length; i++) {
+                // If the value parameter is set, return only the
+                // elements that has the given attribute value.
                 if (value !== null) {
                     if (elements[i].getAttribute(attribute) === value)
                         foundElements.push(elements[i]);
@@ -142,6 +177,60 @@
                 }
             }
 
+            return foundElements;
+        },
+        /**
+         * Same as getElementsByAttribute, but can instead search
+         * for multiple attributes, as set in the attribute param
+         * delimited by a comma.
+         *
+         * @param       elements    Array of elements to search
+         * @param       string      Attribute(s) to find, delmited by a comma.
+         * @param       string      Value to find
+         * @returns     array       List of elements found
+         */
+        getElementsByAttributes: function (elements, attributes, value) {
+            var foundElements = [], value = value || null;
+            // Split the attribute string by the comma-delimiter,
+            // the /\s/g makes sure any whitespace is removed.
+            var attributes = attributes.replace(/\s/g, '').split(",");
+            for (i = 0; i < elements.length; i++) {
+                // Go through each element of the attributes array
+                attributes.map(function(attribute) {
+                    if (value !== null) {
+                        if (elements[i].getAttribute(attribute) === value && foundElements.indexOf(elements[i]) === -1)
+                            foundElements.push(elements[i]);
+                    } else {
+                        if (elements[i].getAttribute(attribute) !== null && foundElements.indexOf(elements[i]) === -1)
+                            foundElements.push(elements[i]);
+                    }
+                });
+            }
+
+            return foundElements;
+        },
+        /**
+         * Find elements by attribute name, and return
+         * them as an object with attribute value as key.
+         *
+         * @param       elements    Array of elements to search
+         * @param       string      Attribute to find
+         * @returns     object      List of elements found
+         */
+        getElementsByAttributeAsObject: function (elements, attribute) {
+            var foundElements = {};
+            for (i = 0; i < elements.length; i++) {
+                // The attribute will also serve as the key
+                // in the object that will be returned.
+                var key = elements[i].getAttribute(attribute);
+                if (key !== undefined && key !== null) {
+                    // Check that the element[key] is an array, otherwise
+                    // the push function will not work.
+                    if (foundElements[key] === undefined || foundElements[key].constructor !== Array)
+                        foundElements[key] = [];
+                    foundElements[key].push(elements[i]);
+                }
+            }
             return foundElements;
         },
         /**
@@ -168,48 +257,78 @@
          */
         getElementsByPattern: function (elements, rule, type) {
             var foundElements = [];
-            // Data-length is a special case, where the placeholders
+            // The length is a special case, where the placeholders
             // of the search pattern (X,Y) first must be replaced by
-            // the values specified in the attribute.
+            // the value(s) specified in the attribute.
             if (type === "length") {
                 for (i = 0; i < elements.length; i++) {
-                    if (elements[i].value.length === 0)
-                        continue;
-                    // Check length attribute specifies both a min and max
-                    // length, if not add a "0," for the regex to work.
-                    var length = elements[i].getAttribute("data-" + type);
-                    if (/^([0-9]\,[0-9]$)/.test(length))
-                        var string = new RegExp(rule.replace("X,Y", length));
-                    else if (/^([0-9]$)/.test(length))
-                        var string = new RegExp(rule.replace("X,Y", "0," + length));
-                    else
-                        // If none of the above checks out, it is not a valid
-                        // value, skip this one and move to the next.
-                        continue;
+                    var length = elements[i].getAttribute("data-length");
+                    var format = elements[i].getAttribute("data-format");
+                    // Format can be either max or min
+                    if (format === "max") {
+                        // The length attribute combined with the format
+                        // attribute must only contain one digit for it
+                        // to be considered valid.
+                        if (/^([0-9]$)/.test(length))
+                            var string = new RegExp(rule.replace("X,Y", "0," + length));
+                        else
+                            continue
+                    } else if (format === "min") {
+                        if (/^([0-9]$)/.test(length))
+                            var string = new RegExp(rule.replace("X,Y", length + ","));
+                        else
+                            continue
+                    } else {
+                        // Even if a format attribute is not set, the validation
+                        // of the element should differ, depending on how the
+                        // length attribute is set up.
+                        if (/^([0-9]\,[0-9]$)/.test(length))
+                            // Normal case, a max and min is specified ("1,5").
+                            var string = new RegExp(rule.replace("X,Y", length));
+                        else if (/^([0-9]\,$)/.test(length))
+                            // A single digit with a comma at the end is to be
+                            // considered minimum length ("1,")
+                            var string = new RegExp(rule.replace("X,Y", length));
+                        else if (/^([0-9]$)/.test(length))
+                            // A single digit without a comma should be treated
+                            // as max length, but for the regex to work a zero
+                            // needs to be added at the beginning. ("5") => ("0,5")
+                            var string = new RegExp(rule.replace("X,Y", "0," + length));
+                        else
+                            // If none of the above checks out it is not a valid
+                            // value, skip this one and move to the next.
+                            continue;
+                    }
+                    // Test the length
                     if (string.test(elements[i].value) === false)
                         foundElements.push(elements[i]);
                 }
+            // Date is also a special case, where the regex pattern
+            // needs to be build depending on the format specified.
             } else if (type === "date") {
+                // The date delimiter can be almost any non-word
+                // character, therefore the regex pattern \W.
                 var delimiterPattern = new RegExp(/([\W])/), delimiterMatches;
                 for (i = 0; i < elements.length; i++) {
                     var elementValue = elements[i].value;
-                    if (elementValue.length === 0)
+                    if (elementValue.length < 1)
                         continue;
                     // Retrieve the date format from the element
-                    var format = elements[i].getAttribute("data-date");
+                    var format = elements[i].getAttribute("data-format");
+                    if (format === null)
+                        format = this.settings.dateFormat;
                     if ((delimiterMatches = delimiterPattern.exec(format)) !== null) {
                         var dateComponents = format.split(delimiterMatches[0]);
                         var dateBrokenDown = elementValue.split(delimiterMatches[0]);
                         var date = [];
-                        // Begin the date pattern
+                        // Begin building the date pattern
                         var validDatePattern = "^(";
-                        // Build the date pattern
                         for (x = 0; x < dateComponents.length; x++) {
                             // Create an array out of the components and inputed date
                             // for later use, to make sure that it is an actual date.
                             var key = dateComponents[x][0].toUpperCase();
                             date[key] = dateBrokenDown[x];
-                            // Dates and months should have a interval length,
+                            // Dates and months should have an interval length,
                             // so that if they are set to max 2 digits (MM) the
                             // user should not have to write a leading zero, as
                             // this often isn't how date parsers read a date.
@@ -219,11 +338,11 @@
                                 var length = dateComponents[x].length;
                             // Keep building that pattern, man.
                             validDatePattern += "\\d{" + length + "}";
-                            // Add the delimiter, but not to the last digit
+                            // Add the delimiter, but not to the last digit.
                             if (dateComponents.length > x+1)
                                 validDatePattern += "\\" + delimiterMatches[0];
                         }
-                        // Close the date pattern
+                        // Close the date pattern.
                         validDatePattern += ")$";
                         // Test the date
                         var validDate = new RegExp(validDatePattern);
@@ -232,7 +351,7 @@
                         } else {
                             // Even if the date has the right format, it may not be an
                             // actual date. This will make sure that the date exists.
-                            // The date must be four digits, so add the first two digits
+                            // The year must be four digits, so add the first two digits
                             // if they are missing.
                             if (date["Y"].length == 2)
                                 date["Y"] = new Date().getFullYear().toString().substr(0,2) + date["Y"];
@@ -242,11 +361,12 @@
                             // Create the date object, and retrieve the individual
                             // components to compare it to the inputed date. If the
                             // date is wrong, the parsed date will not be the same
-                            // as the user inputed one.
+                            // as the user inputed one (JS will parse for example
+                            // 31st of february as a valid date).
                             var dateObject = new Date(date["Y"], (date["M"]-1), date["D"]);
                             var dateParsed = dateObject.getFullYear() +''+ (dateObject.getMonth()+1) +''+ dateObject.getDate();
                             var dateString = date["Y"] +''+ date["M"] +''+ date["D"];
-                            if (dateParsed != dateString)
+                            if (dateParsed !== dateString)
                                 foundElements.push(elements[i]);
                         }
                     }
@@ -258,6 +378,29 @@
                         foundElements.push(elements[i]);
             }
             return foundElements;
+        },
+        /**
+         * Validates a single element. Used as callback on invalidated
+         * elements to check when they are valid.
+         *
+         * @param       element     The element to check.
+         * @returns     bool        True on valid
+         */
+        checkElementByPattern: function (element) {
+            var validationType = element.getAttribute("data-validate");
+            if (validationType === null)
+                validationType = element.getAttribute("required");
+            if (validationType === null)
+                if (element.getAttribute("data-length") !== null)
+                    validationType = "length";
+            if (validationType === null)
+                return null;
+            var elementAsArray = [ element ];
+            var elementPattern = this.patterns[validationType];
+            foundElements = this.getElementsByPattern(elementAsArray, elementPattern, validationType);
+            if (foundElements.length > 0)
+                return false;
+            return true;
         }
     }
 
